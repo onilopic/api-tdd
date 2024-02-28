b10db9e6f7bffc83507bc69fea4be4e9076a6119
Igor Lukinykh <onilopic@gmail.com>
2024-02-28 15:20:57 +0900

    test[api]: Refactor test to use Dataset

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -3,7 +3,6 @@
 use App\Http\Response;
 
 it('returns a correct Response object', function(string $method, string $path, int $statusCode) {
-
     // ARRANGE
     $request = \App\Http\Request::create($method, $path);
     $handler =  static fn() => new Response();
@@ -24,43 +23,8 @@ it('returns a correct Response object', function(string $method, string $path, i
         ->toBeInstanceOf(Response::class)
         ->and($response->getStatusCode())->toBe($statusCode);
 
-});
-
-it('returns a 404 Response object if a route does not exist', function() {
-    $request = \App\Http\Request::create('GET', '/foo');
-    $handler = fn() => new Response();
-    $routeHandlerResolver = Mockery::mock(\App\Routing\RouteHandlerResolver::class);
-    $routeHandlerResolver->shouldReceive('resolve')
-        ->andReturn($handler);
-    $router = new \App\Routing\Router($routeHandlerResolver);
-    $router->setRoutes([
-        ['GET', '/bar', $handler]
-    ]);
-
-    // ACT
-    $response = $router->dispatch($request);
-
-    expect($response)
-        ->toBeInstanceOf(Response::class)
-        ->and($response->getStatusCode())->toBe(Response::HTTP_NOT_FOUND);
-});
-
-it('returns a 405 Response object if a not allowed method is used', function() {
-    // ARRANGE
-    $request = \App\Http\Request::create('GET', '/foo');
-    $handler = static fn() => new Response();
-    $routeHandlerResolver = Mockery::mock(\App\Routing\RouteHandlerResolver::class);
-    $routeHandlerResolver->shouldReceive('resolve')
-        ->andReturn($handler);
-    $router = new \App\Routing\Router($routeHandlerResolver);
-    $router->setRoutes([
-        ['POST', '/foo', $handler]
-    ]);
-
-    // ACT
-    $response = $router->dispatch($request);
-
-    expect($response)
-        ->toBeInstanceOf(Response::class)
-        ->and($response->getStatusCode())->toBe(Response::HTTP_METHOD_NOT_ALLOWED);
-})->todo();
+})->with([
+    '200 OK Response' => ['GET', '/foo', Response::HTTP_OK],
+    '404 Response' => ['GET', '/bar', Response::HTTP_NOT_FOUND],
+    '405 Response' => ['POST', '/foo', Response::HTTP_METHOD_NOT_ALLOWED],
+]);
