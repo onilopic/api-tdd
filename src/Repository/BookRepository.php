9128ad240936bd400b2cd5420ac20ae96871cf86
Igor Lukinykh <onilopic@gmail.com>
2024-02-28 15:49:17 +0900

    test[api]: Add steps to BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -7,5 +7,12 @@ class BookRepository
 
     public function findById(int $id)
     {
+        // Retrieve book data from the database
+
+        // Use it to create and hydrate a Book
+        $book = Book::create();
+
+        // return the Book
+        return $book;
     }
 }
