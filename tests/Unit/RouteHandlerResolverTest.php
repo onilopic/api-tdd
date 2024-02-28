197a16c3513b43cb8ec920e4d9a3038fe1904c7a
Igor Lukinykh <onilopic@gmail.com>
2024-02-28 15:34:33 +0900

    test[api]: Complete RouteHandlerResolverTest

---
+++ tests/Unit/RouteHandlerResolverTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Routing\RouteHandlerResolver;
+
+beforeEach(function() {
+    $this->container = include dirname(__DIR__, 2) . '/config/services.php';
+});
+
+it('resolves the correct route handler closure', function () {
+    // ARRANGE
+    $routeHandlerResolver = new RouteHandlerResolver($this->container);
+    $handlerInfo = static fn() => 'foo';
+
+    // ACT
+    $handler = $routeHandlerResolver->resolve($handlerInfo);
+
+    // ASSERT
+    expect($handler)->toBeCallable()->toBe($handlerInfo);
+});
+
+it('resolves the correct route handler controller', function () {
+    // ARRANGE
+    $routeHandlerResolver = new RouteHandlerResolver($this->container);
+    $handlerInfo = [\App\Controller\BooksController::class, 'show'];
+    // ACT
+    $handler = $routeHandlerResolver->resolve($handlerInfo);
+
+    // ASSERT
+    expect($handler)
+        ->toBeCallable()
+        ->and($handler[0])->toBeInstanceOf(\App\Controller\BooksController::class);
+});
